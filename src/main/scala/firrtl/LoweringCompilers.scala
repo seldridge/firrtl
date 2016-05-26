2bcb650ad2b6249d34995c387389f46b4dec453d
Adam Izraelevitz <izraelevitz@google.com>
2016-05-26 15:50:45 -0700

    Reworked annotation system. Added tenacity and permissibility
    Conflicts:
    	src/main/scala/firrtl/Compiler.scala
    	src/main/scala/firrtl/LoweringCompilers.scala
    	src/main/scala/firrtl/passes/Inline.scala
    	src/test/scala/firrtlTests/AnnotationTests.scala
    	src/test/scala/firrtlTests/InlineInstancesTests.scala

--- src/main/scala/firrtl/LoweringCompilers.scala
+++ src/main/scala/firrtl/LoweringCompilers.scala
@@ -31,6 +31,7 @@ import com.typesafe.scalalogging.LazyLogging
 import java.io.Writer
 import firrtl.passes.Pass
 import firrtl.ir.Circuit
+import Annotations._
 
 // ===========================================
 //              Utility Traits
@@ -64,7 +65,7 @@ class Chisel3ToHighFirrtl () extends Transform with SimpleRun {
       passes.CInferTypes,
       passes.CInferMDir,
       passes.RemoveCHIRRTL)
-   def execute (circuit: Circuit, annotations: Seq[CircuitAnnotation]): TransformResult =
+   def execute (circuit: Circuit, annotationMap: AnnotationMap): TransformResult =
       run(circuit, passSeq)
 }
 
@@ -72,7 +73,7 @@ class Chisel3ToHighFirrtl () extends Transform with SimpleRun {
 //  to a working representation (WIR.scala)
 class IRToWorkingIR () extends Transform with SimpleRun {
    val passSeq = Seq(passes.ToWorkingIR)
-   def execute (circuit: Circuit, annotations: Seq[CircuitAnnotation]): TransformResult =
+   def execute (circuit: Circuit, annotationMap: AnnotationMap): TransformResult =
       run(circuit, passSeq)
 }
 
@@ -91,7 +92,7 @@ class ResolveAndCheck () extends Transform with SimpleRun {
       passes.CheckGenders,
       passes.InferWidths,
       passes.CheckWidths)
-   def execute (circuit: Circuit, annotations: Seq[CircuitAnnotation]): TransformResult =
+   def execute (circuit: Circuit, annotationMap: AnnotationMap): TransformResult =
       run(circuit, passSeq)
 }
 
@@ -106,11 +107,12 @@ class HighFirrtlToMiddleFirrtl () extends Transform with SimpleRun {
       passes.RemoveAccesses,
       passes.ExpandWhens,
       passes.CheckInitialization,
+      passes.ConstProp,
       passes.ResolveKinds,
       passes.InferTypes,
       passes.ResolveGenders,
       passes.InferWidths)
-   def execute (circuit: Circuit, annotations: Seq[CircuitAnnotation]): TransformResult =
+   def execute (circuit: Circuit, annotationMap: AnnotationMap): TransformResult =
       run(circuit, passSeq)
 }
 
@@ -126,7 +128,7 @@ class MiddleFirrtlToLowFirrtl () extends Transform with SimpleRun {
       passes.InferTypes,
       passes.ResolveGenders,
       passes.InferWidths)
-   def execute (circuit: Circuit, annotations: Seq[CircuitAnnotation]): TransformResult =
+   def execute (circuit: Circuit, annotationMap: AnnotationMap): TransformResult =
       run(circuit, passSeq)
 }
 
@@ -147,7 +149,7 @@ class EmitVerilogFromLowFirrtl (val writer: Writer) extends Transform with Simpl
       passes.CommonSubexpressionElimination,
       passes.DeadCodeElimination,
       passes.VerilogRename)
-   def execute (circuit: Circuit, annotations: Seq[CircuitAnnotation]): TransformResult = {
+   def execute (circuit: Circuit, annotationMap: AnnotationMap): TransformResult = {
       val result = run(circuit, passSeq)
       (new VerilogEmitter).run(result.circuit, writer)
       result
@@ -157,7 +159,7 @@ class EmitVerilogFromLowFirrtl (val writer: Writer) extends Transform with Simpl
 // Emits Firrtl.
 // Operates on WIR/IR nodes.
 class EmitFirrtl (val writer: Writer) extends Transform {
-   def execute (circuit: Circuit, annotations: Seq[CircuitAnnotation]): TransformResult = {
+   def execute (circuit: Circuit, annotationMap: AnnotationMap): TransformResult = {
       FIRRTLEmitter.run(circuit, writer)
       TransformResult(circuit)
    }
@@ -182,7 +184,7 @@ class LowFirrtlCompiler extends Compiler {
    def transforms(writer: Writer): Seq[Transform] = Seq(
       new Chisel3ToHighFirrtl(),
       new IRToWorkingIR(),
-      passes.InlineInstances,
+      new passes.InlineInstances(TransID(0)),
       new ResolveAndCheck(),
       new HighFirrtlToMiddleFirrtl(),
       new MiddleFirrtlToLowFirrtl(),
@@ -198,7 +200,7 @@ class VerilogCompiler extends Compiler {
       new ResolveAndCheck(),
       new HighFirrtlToMiddleFirrtl(),
       new MiddleFirrtlToLowFirrtl(),
-      passes.InlineInstances,
+      new passes.InlineInstances(TransID(0)),
       new EmitVerilogFromLowFirrtl(writer)
    )
 }
