f72313db6fd0b8f7706bf1705470dc58d3caa9df
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2018-08-22 16:08:00 -0400

    Make ClockListAnnotation a RegisteredTransform
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- src/main/scala/firrtl/passes/clocklist/ClockListTransform.scala
+++ src/main/scala/firrtl/passes/clocklist/ClockListTransform.scala
@@ -14,6 +14,8 @@ import Utils._
 import memlib.AnalysisUtils._
 import memlib._
 import Mappers._
+import firrtl.options.RegisteredTransform
+import scopt.OptionParser
 
 case class ClockListAnnotation(target: ModuleName, outputConfig: String) extends
     SingleTargetAnnotation[ModuleName] {
@@ -54,9 +56,19 @@ Usage:
   }
 }
 
-class ClockListTransform extends Transform {
+class ClockListTransform extends Transform with RegisteredTransform {
   def inputForm = LowForm
   def outputForm = LowForm
+
+  def addOptions(parser: OptionParser[AnnotationSeq]): Unit = parser
+    .opt[String]("list-clocks")
+    .abbr("clks")
+    .valueName ("-c:<circuit>:-m:<module>:-o:<filename>")
+    .action( (x, c) => c ++ Seq(passes.clocklist.ClockListAnnotation.parse(x),
+                                RunFirrtlTransformAnnotation(classOf[ClockListTransform])) )
+    .maxOccurs(1)
+    .text("List which signal drives each clock of every descendent of specified module")
+
   def passSeq(top: String, writer: Writer): Seq[Pass] =
     Seq(new ClockList(top, writer))
   def execute(state: CircuitState): CircuitState = {
